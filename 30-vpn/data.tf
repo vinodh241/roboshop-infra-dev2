59e9594d70ea73b764cc9d1ab34a9d840694c295
vinodh241 <a.vinodh9072@gmail.com>
2026-01-06 10:49:42 +0530

    terraform-roboshop-infra

--- 30-vpn/data.tf
+++ 30-vpn/data.tf
@@ -1,10 +1,11 @@
 data "aws_ami" "openvpn" {
   owners           = ["679593333241"]
+ 
   most_recent      = true
 
   filter {
     name   = "name"
-    values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
+    values = ["OpenVPN Access Server Community Image-8fbe3379-63b6-43e8-87bd-0e93fd7be8f3"]
   }
 
   filter {
