a7aaa57404e8af4cc1c832fea2092937ad5e5a23
vinodh241 <a.vinodh9072@gmail.com>
2026-01-23 11:16:17 +0530

    roboshop-infra-dev

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/vinodh241/terraform-aws-roboshop.git?ref=main" ## Taking the modules form git 
+    source = "git::https://github.com/vinodh241/terraform-aws-roboshop.git?ref=main" ## Taking the modules form git
     component = each.key
     rule_priority = each.value.rule_priority
 }
