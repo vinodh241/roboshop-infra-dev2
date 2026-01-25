c6c04247290dc0df0f7ff68de16049b681766a59
vinodh241 <a.vinodh9072@gmail.com>
2026-01-25 17:08:40 +0530

    roboshop-infra-dev

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
 
-  # need more for terraform
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp3" # or "gp2", depending on your preference
-  }
-  user_data = file("bastion.sh")
-  iam_instance_profile = "TerraformAdmin"
+  # # need more for terraform
+  # root_block_device {
+  #   volume_size = 50
+  #   volume_type = "gp3" # or "gp2", depending on your preference
+  # }
+  # user_data = file("bastion.sh")
+  # iam_instance_profile = "TerraformAdmin"
 
   
   tags = merge(
